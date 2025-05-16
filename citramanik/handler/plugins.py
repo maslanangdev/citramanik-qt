fab321f87dd745e67d71d7fe30a00f212c83391d
Mas RJ95 <48236174+maslanangdev@users.noreply.github.com>
2025-05-17 00:46:50 +0700

    skeleton code for later implementations

--- citramanik/handler/plugins.py
+++ citramanik/handler/plugins.py
@@ -11,3 +11,15 @@ class CitramanikPluginInterface:
     @classmethod
     def execute(cls, *args, **kwargs):
         raise NotImplementedError
+    
+    def call_inkscape():
+        raise NotImplementedError
+
+    def call_parser():
+        raise NotImplementedError
+
+    def call_cmd():
+        raise NotImplementedError
+    
+    def get_input_data():
+        raise NotImplementedError
